1752d7e501dc1814f687d02180c7d98086c871b4
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-01-31 22:56:07 +0100

    Add Page Object Model class for Login page

--- src/test/java/pageObjectModel/PageObjectModel.java
+++ src/test/java/pageObjectModel/PageObjectModel.java
@@ -1,2 +1,48 @@
-package pageObjectModel;public class PageObjectModel {
+package pageObjectModel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageObjectModel {
+
+    public WebDriver driver;
+
+    PageObjectModel(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    //WebElements locators + identification
+    @FindBy(id = "site-name")
+    WebElement siteName;
+    @FindBy(name = "uid")
+    WebElement textUserID;
+    @FindBy(name = "password")
+    WebElement textPassword;
+    @FindBy(name = "btnLogin")
+    WebElement buttonLogin;
+
+    public void setUserID(String idUser)
+    {
+        textUserID.sendKeys(idUser);
+    }
+
+    public void setPassword(String password)
+    {
+        textPassword.sendKeys(password);
+    }
+
+    public void clickLogin()
+    {
+        buttonLogin.click();
+    }
+
+    public boolean checkLoginPresents()
+    {
+        boolean status = siteName.isDisplayed();
+        return status;
+    }
 }
